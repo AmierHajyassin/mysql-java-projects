a3460bf49e8e4de9acc4d0636a74e2223c471a52
AmierHajyassin <amierhajyassin89@yahoo.com>
2023-03-11 17:30:22 -0500

    week 10 assignment

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -11,18 +11,22 @@ import projects.service.ProjectService;
 public class ProjectsApp {
 	private Scanner scanner = new Scanner(System.in);
 	private ProjectService projectService = new ProjectService();
+	private Project curProject;
 
 	// @formatter:off
 	  private List<String> operations = List.of(
-	      "1) Add a project"
+	      "1) Add a project",
+	      "2) List projects",
+	      "3) Select project"
 	      );
 	  // @formatter:on
 
 	public static void main(String[] args) {
-		new ProjectsApp().processUserSelections();
+
 
 	}
 
+	@SuppressWarnings("unused")
 	private void processUserSelections() {
 		boolean done = false;
 
@@ -38,8 +42,13 @@ public class ProjectsApp {
 				case 1:
 					createProject();
 					break;
-
-				default:
+				case 2:
+					listProjects();
+				case 3:
+					selectProject();
+					break;
+					
+					default:
 					System.out.println("\n" + selection + " is not a valid selection. Try again.");
 				}
 
@@ -48,6 +57,26 @@ public class ProjectsApp {
 			}
 		}
 
+	}
+
+	private void selectProject() {
+		listProjects();
+		
+		Integer projectId = getIntInput("Select a project ID to select a project");
+		curProject = null;
+		
+		curProject = projectService.fetchProjectById(projectId);
+		
+		
+	}
+	
+	
+	
+	private void listProjects() {
+		List<Project> projects = projectService.fetchAllProjects();
+		System.out.println("\nProjects: ");
+		projects.forEach(project -> System.out.println(" " 
+		+ project.getProjectId() + ": "+ project.getProjectName()));
 	}
 
 	private void createProject() {
--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -1,15 +1,33 @@
 package projects.service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
 import projects.dao.ProjectDao;
 import projects.entity.Project;
 
 public class ProjectService {
-	
-	
-	private ProjectDao projectDao = new ProjectDao();
-	  
-	  public Project addProject(Project project) {
-	    return projectDao.insertProject(project);
-	  }
 
+	private ProjectDao projectDao = new ProjectDao();
+
+	public Project addProject(Project project) {
+		return projectDao.insertProject(project);
+	}
+
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects()
+		// @formatter:off
+					.stream()
+					.sorted((p1, p2) -> p1.getProjectId() - p2.getProjectId())
+					.collect(Collectors.toList());
+				// @formatter:on
+
+	}
+
+	public Project fetchProjectById(Integer projectId) {
+		return projectDao.fetchProjectById(projectId).orElseThrow(() -> 
+				new NoSuchElementException("Project with project Id=" +
+											projectId +"does not exist." ));
+	}
 }
